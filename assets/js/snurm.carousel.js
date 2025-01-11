0138246b363afac1a756a88c76a6e302dede1007
Sadik Mohamud <msadiknur@gmail.com>
2025-01-11 19:02:19 +0000

    Update snurm.carousel.js

--- assets/js/snurm.carousel.js
+++ assets/js/snurm.carousel.js
@@ -6,10 +6,10 @@ const albums = [
       description: 'Football club photos and memories',
       imageCount: 4,
       imagePaths: [
-          '.../assets/images/face-paint (2).webp',
-          '.../assets/images/face-paint (3).webp',
-          '.../assets/images/face-paint (4).webp',
-          '.../assets/images/face-paint (5).webp'
+          './assets/images/face-paint (2).webp',
+          './assets/images/face-paint (3).webp',
+          './assets/images/face-paint (4).webp',
+          './assets/images/face-paint (5).webp'
       ]
   },
   {
@@ -18,16 +18,16 @@ const albums = [
       description: 'Various events and excursions',
       imageCount: 10,
       imagePaths: [
-          '.../assets/images/191.webp',
-          '.../assets/images/232.webp',
-          '.../assets/images/317.webp',
-          '.../assets/images/544.webp',
-          '.../assets/images/554.webp',
-          '.../assets/images/574.webp',
-          '.../assets/images/581.webp',
-          '.../assets/images/787.webp',
-          '.../assets/images/864.webp',
-          '.../assets/images/93.webp'
+          './assets/images/191.webp',
+          './assets/images/232.webp',
+          './assets/images/317.webp',
+          './assets/images/544.webp',
+          './assets/images/554.webp',
+          './assets/images/574.webp',
+          './assets/images/581.webp',
+          './assets/images/787.webp',
+          './assets/images/864.webp',
+          './assets/images/93.webp'
       ]
   },
   {
@@ -36,15 +36,15 @@ const albums = [
       description: 'Educational activities and achievements',
       imageCount: 10,
       imagePaths: [
-          '.../assets/images/ROA.webp',
-          '.../assets/images/ROA1.webp',
-          '.../assets/images/ROA2.webp',
-          '.../assets/images/ROA3.webp',
-          '.../assets/images/ROA4.webp',
-          '.../assets/images/ROA5.webp',
-          '.../assets/images/ROA7.jpg',
-          '.../assets/images/ROA8.jpg',
-          '.../assets/images/ROA10.jpg'
+          './assets/images/ROA.webp',
+          './assets/images/ROA1.webp',
+          './assets/images/ROA2.webp',
+          './assets/images/ROA3.webp',
+          './assets/images/ROA4.webp',
+          './assets/images/ROA5.webp',
+          './assets/images/ROA7.jpg',
+          './assets/images/ROA8.jpg',
+          './assets/images/ROA10.jpg'
       ]
   }
 ];
